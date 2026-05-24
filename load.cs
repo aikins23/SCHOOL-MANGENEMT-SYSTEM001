ce66dfa3e115f264030e08cc02d8e8209b85fad5
@aikins23 <buabengaikinsemmanuel@gmail.com>
2026-05-24 09:18:08 +0000

    feat: implement animation system with background image and timer logic

--- load.cs
+++ load.cs
@@ -13,6 +13,11 @@ namespace kingdom_Preparatory_School_Management_System
     public partial class load : Form
     {
         private Timer timer1;
+        private Timer animationTimer;
+        private int dotAnimationTicks = 0;
+        private int dotOpacityPhase = 0;
+        private int totalLoadingTicks = 0;
+        private int maxLoadingTicks = 100; // ~5 seconds at 50ms
 
         public load()
         {
@@ -31,7 +36,77 @@ namespace kingdom_Preparatory_School_Management_System
 
         private void load_Load(object sender, EventArgs e)
         {
-            // Any initialization code for the form load event
+            try
+            {
+                this.BackgroundImage = Properties.Resources.school_background;
+            }
+            catch
+            {
+                this.BackColor = System.Drawing.Color.White;
+            }
+            InitializeSplashAnimation();
+        }
+
+        private void InitializeSplashAnimation()
+        {
+            animationTimer = new Timer();
+            animationTimer.Interval = 50;
+            animationTimer.Tick += AnimationTimer_Tick;
+            animationTimer.Start();
+            dotAnimationTicks = 0;
+            totalLoadingTicks = 0;
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            dotAnimationTicks++;
+            totalLoadingTicks++;
+
+            int phasePosition = dotAnimationTicks % 24;
+            UpdateDotAnimation(phasePosition);
+
+            if (totalLoadingTicks >= maxLoadingTicks)
+            {
+                animationTimer.Stop();
+                FadeOutAndShowLogin();
+            }
+        }
+
+        private void UpdateDotAnimation(int phasePosition)
+        {
+            if (phasePosition >= 0 && phasePosition <= 23)
+            {
+                if (Controls.ContainsKey("labelLoadingDots"))
+                {
+                    Control labelLoadingDots = Controls["labelLoadingDots"];
+                    labelLoadingDots.Text = "● ● ● ●";
+                    labelLoadingDots.Visible = true;
+                }
+            }
+        }
+
+        private void FadeOutAndShowLogin()
+        {
+            Timer fadeTimer = new Timer();
+            fadeTimer.Interval = 30;
+            int opacityLevel = 10;
+
+            fadeTimer.Tick += (s, e) =>
+            {
+                opacityLevel -= 1;
+                if (opacityLevel <= 0)
+                {
+                    fadeTimer.Stop();
+                    new frmlogin().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    this.Opacity = opacityLevel / 10.0;
+                }
+            };
+
+            fadeTimer.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
